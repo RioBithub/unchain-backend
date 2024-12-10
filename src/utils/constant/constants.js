7df0047d1d756f15cb94764058f9da6c99cadf4a
Hidayatus Sholikhin <hidaysco26@gmail.com>
2024-12-10 13:16:45 +0700

    add: sugar level classification, get user profile

--- src/utils/constant/constants.js
+++ src/utils/constant/constants.js
@@ -2,5 +2,8 @@ import { config } from "dotenv";
 config()
 
 export const CONFIG = {
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  MODEL_SUGAR_LEVEL_URL: process.env.MODEL_SUGAR_LEVEL_URL
 }
+
+export const SUGAR_LEVEL = ['High', 'Low', 'Normal'];
